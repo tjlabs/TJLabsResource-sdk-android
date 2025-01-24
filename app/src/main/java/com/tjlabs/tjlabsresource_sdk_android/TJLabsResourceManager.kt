b321df75021c998a5b9ea019279fa6cf84d167b2
Henry(Changsoo Yu) <147014260+Changsoo-Yu@users.noreply.github.com>
2025-01-24 10:30:34 +0900

    rename:service name
    jupiter-> navigation

--- app/src/main/java/com/tjlabs/tjlabsresource_sdk_android/TJLabsResourceManager.kt
+++ app/src/main/java/com/tjlabs/tjlabsresource_sdk_android/TJLabsResourceManager.kt
@@ -3,19 +3,19 @@ package com.tjlabs.tjlabsresource_sdk_android
 import android.app.Application
 import android.content.Context
 import android.content.SharedPreferences
-import android.util.Log
 
 class TJLabsResourceManager(private val application: Application) {
+    //TODO() 네트워크 상황 고려하여 진행 및 다운로드 요청
     private val sharedPrefs: SharedPreferences = application.getSharedPreferences("TJLabsResourcesPref", Context.MODE_PRIVATE)
     private val pathPixelManager = TJLabsPathPixelManager(application, sharedPrefs)
+    private var region = JupiterRegion.KOREA
+    private var sectorId = 0
 
     companion object {
-        const val SERVICE_JUPITER = "jupiter"
+        const val SERVICE_NAVI = "navigation"
         const val SERVICE_MAP = "map"
-        const val REGION_KOREA = "korea"
-        const val REGION_US = "US"
-        const val REGION_CANADA = "Canada"
         const val PATH_PIXEL_KEY_NAME = "path-pixel"
+        val ppDataMap : MutableMap<String, PathPixelData> = mutableMapOf()
 
         fun getResourceDirInPrefs(sharedPrefs: SharedPreferences, key: String) : String? {
             val prefKey = "${key}_dir"
@@ -43,11 +43,13 @@ class TJLabsResourceManager(private val application: Application) {
         // 1. 서버에 해당 섹터가 어떤 서비스를 가지고 있는지 요청하기
         // 2. SharedPref에 저징된 리소스별 버젼을 통해 업데이트 필요한 리소스 확인하기
         // 3. 서버 요청 및 버젼 업데이트 및 저장
+        this.region = region
+        this.sectorId = sectorId
         TJLabsResourceNetworkConstant.setServerURL(region)
         val sectorServiceList = getSectorServiceFromServer(region, sectorId) // 됐다고 가정
         for (service in sectorServiceList) {
             when (service) {
-                SERVICE_JUPITER -> {
+                SERVICE_NAVI -> {
                     pathPixelManager.updatePathPixel(region, sectorId) { bool, msg ->
                         completion(bool, msg)
                     }
@@ -65,13 +67,6 @@ class TJLabsResourceManager(private val application: Application) {
     private fun getSectorServiceFromServer(region: String, sectorId: Int) : List<String> {
         //TODO()
         //서버로 부터 해당 리젼의 섹터가 어떤 서비스를 하는 지 요청하고, List<String>으로 전달받음
-        return listOf("jupiter", "map")
+        return listOf("navigation", "map")
     }
-
-
-    fun returnPathPixelData() : MutableMap<String, PathPixelData> {
-        return pathPixelManager.returnPathPixelData()
-    }
-
-
 }
