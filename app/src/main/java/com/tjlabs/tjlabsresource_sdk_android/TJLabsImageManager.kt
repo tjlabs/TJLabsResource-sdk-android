332e972bba7cf50773d00188810f6377d5fbd816
Henry(Changsoo Yu) <147014260+Changsoo-Yu@users.noreply.github.com>
2025-02-19 10:06:48 +0900

    feat:ImageManager

--- app/src/main/java/com/tjlabs/tjlabsresource_sdk_android/TJLabsImageManager.kt
+++ app/src/main/java/com/tjlabs/tjlabsresource_sdk_android/TJLabsImageManager.kt
@@ -1,4 +1,30 @@
 package com.tjlabs.tjlabsresource_sdk_android
 
+import com.tjlabs.tjlabsresource_sdk_android.TJLabsBuildingLevelManager.Companion.buildingLevelDataMap
+import com.tjlabs.tjlabsresource_sdk_android.TJLabsResourceNetworkConstant.getLevelServerVersion
+import com.tjlabs.tjlabsresource_sdk_android.TJLabsResourceNetworkConstant.getUserBaseURL
+
 class TJLabsImageManager {
+    companion object {
+        var isPerformed = false
+    }
+
+    fun loadBuildingLevel(region: String, sectorId: Int, completion: (Boolean, Map<String, List<String>>) -> Unit) {
+        val result = mutableMapOf<String, MutableList<String>>()
+
+        buildingLevelDataMap[sectorId]?.let {
+            completion(true, it)
+        } ?: run {
+            val input = SectorIdInput(sectorId)
+            TJLabsResourceNetworkManager.postBuildingLevel(
+                getUserBaseURL(), input, getLevelServerVersion()
+            ) { statusCode, buildingLevelList ->
+                if (statusCode == 200) {
+                } else {
+                    println("(TJLabsResource) Fail : loadBuildingLevel")
+                    completion(false, result)
+                }
+            }
+        }
+    }
 }
