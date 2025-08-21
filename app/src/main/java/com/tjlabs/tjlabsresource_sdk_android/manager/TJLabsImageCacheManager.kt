979985d7143a0270da06fc39d8098570163eab46
Henry(Changsoo Yu) <147014260+Changsoo-Yu@users.noreply.github.com>
2025-08-21 17:56:06 +0900

    Create TJLabsImageCacheManager.kt

---
+++ app/src/main/java/com/tjlabs/tjlabsresource_sdk_android/manager/TJLabsImageCacheManager.kt
@@ -0,0 +1,28 @@
+package com.tjlabs.tjlabsresource_sdk_android.manager
+
+import android.graphics.Bitmap
+import android.util.LruCache
+
+class TJLabsImageCacheManager private constructor() {
+
+    companion object {
+        @Volatile
+        private var INSTANCE: TJLabsImageCacheManager? = null
+
+        fun getInstance(): TJLabsImageCacheManager {
+            return INSTANCE ?: synchronized(this) {
+                INSTANCE ?: TJLabsImageCacheManager().also { INSTANCE = it }
+            }
+        }
+    }
+
+    private val cache: LruCache<String, Bitmap> = LruCache(50) // 최대 50개의 이미지를 캐싱
+
+    fun putBitmap(key: String, bitmap: Bitmap) {
+        cache.put(key, bitmap)
+    }
+
+    fun getBitmap(key: String): Bitmap? {
+        return cache.get(key)
+    }
+}
