ae20fb65d583a5b7a2e74d7874638fe79ed1a8ae
Henry(Changsoo Yu) <147014260+Changsoo-Yu@users.noreply.github.com>
2025-08-21 11:13:24 +0900

    feat:update TJLabsBuildingsManager

---
+++ app/src/main/java/com/tjlabs/tjlabsresource_sdk_android/manager/TJLabsBuildingsManager.kt
@@ -0,0 +1,39 @@
+package com.tjlabs.tjlabsresource_sdk_android.manager
+import com.tjlabs.tjlabsresource_sdk_android.BuildingOutput
+import com.tjlabs.tjlabsresource_sdk_android.ResourceRegion
+import com.tjlabs.tjlabsresource_sdk_android.util.Logger
+
+internal interface BuildingsDelegate {
+    fun onBuildingsData(data: List<BuildingOutput>)
+}
+
+internal class TJLabsBuildingsManager {
+    companion object {
+        val buildingsDataMap: MutableMap<Int, List<BuildingOutput>> = mutableMapOf()
+        val levelIdMap: MutableMap<String, Int> = mutableMapOf()
+        val levelImageUrlMap: MutableMap<String, String> = mutableMapOf()
+    }
+
+    var delegate: BuildingsDelegate? = null
+    private var region: String = ResourceRegion.KOREA.value
+
+    fun setRegion(region: String) {
+        this.region = region
+    }
+
+    fun setBuildings(sectorId: Int, buildings: List<BuildingOutput>) {
+        buildingsDataMap[sectorId] = buildings
+        for (building in buildings) {
+            for (level in building.levels) {
+                val key = "${sectorId}_${building.name}_${level.name}"
+                levelIdMap[key] = level.id
+                levelImageUrlMap[key] = level.image
+            }
+        }
+        delegate?.onBuildingsData(buildings)
+    }
+
+    fun getBuildings(sectorId: Int): List<BuildingOutput>? {
+        return buildingsDataMap[sectorId]
+    }
+}
