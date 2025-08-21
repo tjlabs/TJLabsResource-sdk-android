55702e8cc00ca0a3544c627ef4d7b721bdf06ca4
Henry(Changsoo Yu) <147014260+Changsoo-Yu@users.noreply.github.com>
2025-08-21 18:04:05 +0900

    Update MainActivity.kt

--- app/src/main/java/com/tjlabs/tjlabsresource_sdk_android/MainActivity.kt
+++ app/src/main/java/com/tjlabs/tjlabsresource_sdk_android/MainActivity.kt
@@ -8,6 +8,10 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.tjlabs.tjlabsauth_sdk_android.TJLabsAuthManager
+import com.tjlabs.tjlabsresource_sdk_android.manager.EntranceErrorType
+import com.tjlabs.tjlabsresource_sdk_android.manager.ParamErrorType
+import com.tjlabs.tjlabsresource_sdk_android.manager.TJLabsResourceManager
+import com.tjlabs.tjlabsresource_sdk_android.util.Logger
 
 class MainActivity : AppCompatActivity(), TJLabsResourceManagerDelegate {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,92 +29,97 @@ class MainActivity : AppCompatActivity(), TJLabsResourceManagerDelegate {
         }
 
         // sector id 1 = Tips town
+        // coex id 7
         TJLabsAuthManager.initialize(application)
         TJLabsAuthManager.setServerURL("jupiter")
         TJLabsAuthManager.auth(tenantId, tenantPw) {
                 code, success->
-            Log.d("VenusServiceResult", "code : $code // auth : $success")
-
             if (success) {
                 val tjlabsResourceManager = TJLabsResourceManager()
                 tjlabsResourceManager.delegate = this
-                tjlabsResourceManager.loadJupiterResources(application, ResourceRegion.KOREA, 1)
+                tjlabsResourceManager.setDebugOption(true)
+//                tjlabsResourceManager.loadJupiterResource(application, ResourceRegion.KOREA.value, 7)
+
+                tjlabsResourceManager.loadMapResource(application, ResourceRegion.KOREA.value, 7)
             }
         }
     }
 
-    override fun onBuildingLevelData(
-        isOn: Boolean,
-        buildingLevelData: Map<String, List<String>>
-    ) {
-
-        Log.d(TAG, "isOn : $isOn // buildingLevelData : $buildingLevelData")
+    override fun onSectorData(data: SectorOutput) {
+        Logger.d("onSectorData : $data")
 
     }
 
-    override fun onPathPixelData(
-        isOn: Boolean,
-        pathPixelKey: String,
-        data: PathPixelData?
-    ) {
-        Log.d(TAG, "isOn : $isOn // pathPixelKey : $pathPixelKey // data : $data")
+    override fun onSectorError() {
+        Logger.d("onSectorError")
 
     }
 
-    override fun onPathPixelDataLoaded(
-        isOn: Boolean,
-        pathPixelKey: String,
-        data: PathPixelDataIsLoaded?
-    ) {
-        Log.d(TAG, "PathPixel Loaded isOn : $isOn // pathPixelKey : $pathPixelKey // data : $data")
+    override fun onBuildingsData(data: List<BuildingOutput>) {
+         Logger.d("onBuildingsData : $data")
     }
 
-    override fun onBuildingLevelImageData(
-        isOn: Boolean,
-        imageKey: String,
-        data: Bitmap?
-    ) {
-        Log.d(TAG, "isOn : $isOn // imageKey : $imageKey // data : $data")
-
+    override fun onScaleOffsetData(scaleKey: String, data: List<Float>) {
+        Logger.d("onScaleOffsetData : $scaleKey // data : $data")
     }
 
-    override fun onScaleOffsetData(
-        isOn: Boolean,
-        scaleKey: String,
-        data: List<Float>
-    ) {
-        Log.d(TAG, "isOn : $isOn // scaleKey : $scaleKey // data : $data")
-
+    override fun onScaleOffsetError(scaleKey: String) {
+        Logger.d("onScaleOffsetError : $scaleKey")
     }
 
-    override fun onEntranceRouteData(
-        isOn: Boolean,
-        entranceKey: String,
-        data: EntranceRouteData?
-    ) {
-        Log.d(TAG, "isOn : $isOn // entrance route Key : $entranceKey// data : $data")
-
+    override fun onPathPixelData(pathPixelKey: String, data: PathPixelData) {
+        Logger.d("onPathPixelData : $pathPixelKey // data : $data")
     }
 
-    override fun onEntranceData(isOn: Boolean, entranceKey: String, data: EntranceData?) {
-        Log.d(TAG, "isOn : $isOn // entranceKey : $entranceKey// data : $data")
+    override fun onPathPixelError(pathPixelKey: String) {
+        Logger.d("onPathPixelData : $pathPixelKey")
     }
 
-    override fun onUnitData(isOn: Boolean, unitKey: String, data: List<UnitData>?) {
-        Log.d(TAG, "isOn : $isOn // unit : $data")
+    override fun onGeofenceData(geofenceKey: String, data: GeofenceData) {
+        Logger.d("onGeofenceData : $geofenceKey // data : $data")
     }
 
-    override fun onParamData(isOn: Boolean, data: ParameterData?) {
-        Log.d(TAG, "isOn : $isOn // param : $data")
+    override fun onGeofenceError(geofenceKey: String) {
+        Logger.d("onGeofenceError : $geofenceKey")
     }
 
-    override fun onGeofenceData(isOn: Boolean, key: String, data: GeofenceData?) {
-        Log.d(TAG, "isOn : $isOn // geo key : $key")
-
+    override fun onEntranceData(entranceKey: String, data: EntranceData) {
+        Logger.d("onEntranceData : $entranceKey // data : $data")
     }
 
-    override fun onError(error: ResourceError) {
+    override fun onEntranceRouteData(entranceKey: String, data: EntranceRouteData) {
+        Logger.d("onEntranceRouteData : $entranceKey // data : $data")
     }
 
+    override fun onEntranceError(type: EntranceErrorType, entranceKey: String) {
+        Logger.d("onEntranceError type $type // $entranceKey")
+    }
 
+    override fun onSectorParamData(data: SectorParameterOutput) {
+        Logger.d("onSectorParamData data $data")
+    }
+
+    override fun onLevelParamData(paramKey: String, data: LevelParameterOutput) {
+        Logger.d("onLevelParamData type $paramKey // $data")
+    }
+
+    override fun onParamError(type: ParamErrorType, paramKey: String?) {
+        Logger.d("onParamError type $type // $paramKey")
+    }
+
+    override fun onBuildingLevelImageData(imageKey: String, data: Bitmap?) {
+        Logger.d("onBuildingLevelImageData imageKey $imageKey // $data")
+    }
+
+    override fun onBuildingLevelImageError(imageKey: String) {
+        Logger.d("onBuildingLevelImageData imageKey $imageKey")
+    }
+
+    override fun onUnitData(unitKey: String, data: List<UnitData>?) {
+        Logger.d("onUnitData unitKey $unitKey // data : $data")
+    }
+
+    override fun onUnitDataError(unitKey: String) {
+        Logger.d("onUnitDataError unitKey $unitKey")
+    }
 }
