6ca3dacd7e8e225ab2da91e7123db842b4251ba9
Henry(Changsoo Yu) <147014260+Changsoo-Yu@users.noreply.github.com>
2025-08-21 18:04:01 +0900

    Update TJLabsSectorManager.kt

--- app/src/main/java/com/tjlabs/tjlabsresource_sdk_android/manager/TJLabsSectorManager.kt
+++ app/src/main/java/com/tjlabs/tjlabsresource_sdk_android/manager/TJLabsSectorManager.kt
@@ -1,7 +1,5 @@
 package com.tjlabs.tjlabsresource_sdk_android.manager
 
-import android.util.Log
-import com.tjlabs.tjlabsresource_sdk_android.BuildingOutput
 import com.tjlabs.tjlabsresource_sdk_android.ResourceRegion
 import com.tjlabs.tjlabsresource_sdk_android.SectorIdInput
 import com.tjlabs.tjlabsresource_sdk_android.SectorOutput
